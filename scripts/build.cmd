ee5e1d991a9c1afe621e493340154de2c4969045
lupyuen <luppy@appkaki.com>
2019-07-22 09:12:42 +0800

    Fixed pflag

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,8 +1,8 @@
 cd newt
-go get github.com/spf13/pflag
-go get github.com/spf13/cobra
-go get mynewt.apache.org/newt/util
-go get mynewt.apache.org/newt/newt/cli
-go get mynewt.apache.org/newt/newt/newtutil
-go get mynewt.apache.org/newt/newt/settings
+go get -u github.com/spf13/pflag
+go get -u github.com/spf13/cobra
+go get -u mynewt.apache.org/newt/util
+go get -u mynewt.apache.org/newt/newt/cli
+go get -u mynewt.apache.org/newt/newt/newtutil
+go get -u mynewt.apache.org/newt/newt/settings
 go build

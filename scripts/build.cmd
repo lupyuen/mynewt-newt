53b246ceb0538835a62ec11a24aa775f0cd61734
lupyuen <luppy@appkaki.com>
2019-07-22 09:05:45 +0800

    Adding build script for Windows

---
+++ scripts/build.cmd
@@ -0,0 +1,7 @@
+cd newt
+go get github.com/spf13/cobra
+go get mynewt.apache.org/newt/util
+go get mynewt.apache.org/newt/newt/cli
+go get mynewt.apache.org/newt/newt/newtutil
+go get mynewt.apache.org/newt/newt/settings
+go build

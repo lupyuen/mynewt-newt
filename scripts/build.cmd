a519591400c60ae207d23609df6c225b831d18d3
Lee Lup Yuen <luppy@appkaki.com>
2019-07-24 13:28:00 +0800

    Update build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,8 +1,10 @@
+::  Uncomment these lines for the very first build...
+::  go get -u github.com/spf13/pflag
+::  go get -u github.com/spf13/cobra
+::  go get -u mynewt.apache.org/newt/util
+::  go get -u mynewt.apache.org/newt/newt/cli
+::  go get -u mynewt.apache.org/newt/newt/newtutil
+::  go get -u mynewt.apache.org/newt/newt/settings
+
 cd newt
-::go get -u github.com/spf13/pflag
-::go get -u github.com/spf13/cobra
-::go get -u mynewt.apache.org/newt/util
-::go get -u mynewt.apache.org/newt/newt/cli
-::go get -u mynewt.apache.org/newt/newt/newtutil
-::go get -u mynewt.apache.org/newt/newt/settings
 go build
